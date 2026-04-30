a0cc9283a7eac7421a568e8a8554f603819f7b61
Prottoy Mankin <prottoy032020@gmail.com>
2026-04-30 15:18:35 +0600

    Dynamically show product cards in products page

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,10 +1,27 @@
+"use client"
+
+import ProductCard from '@/components/shared/ProductCard';
+import useProducts from '@/hooks/useProducts'
 import React from 'react'
 
 const ProductsPage = () => {
+  const { products, isLoading } = useProducts(); 
+
   return (
-    <div>
-      This is product page
-    </div>
+    <section className='max-w-7xl mx-auto space-y-10 py-10'>
+      <h2 className='font-bold text-3xl text-neutral-800'>All Products</h2>
+
+      <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-6">
+        {
+          products.map(product => (
+            <ProductCard
+              key={product.id}
+              product={product}
+            />
+          ))
+        }
+      </div>
+    </section>
   )
 }
 
--- src/components/HomePage/Banner.jsx
+++ src/components/HomePage/Banner.jsx
@@ -11,7 +11,7 @@ const Banner = () => {
       className=" bg-linear-to-r from-[#FFF7CC] to-[#FFE8A3] min-h-[60vh] flex flex-col lg:flex-row gap-5 items-center justify-around my-10 p-6 rounded-2xl text-neutral-800"
     >
       <div className="flex flex-col gap-6 items-center lg:items-start">
-        <div className="space-y-2">
+        <div className="space-y-2 text-center lg:text-left">
           <h1 className="font-semibold text-5xl">SUMMER SALE ☀️</h1>
           <h2 className="font-medium text-2xl">Up to 50% OFF on Summer Essentials🔥</h2>
         </div>

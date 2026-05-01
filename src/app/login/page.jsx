4d5a5d78789397469c9dbc2b133b96e6d3a2e665
Prottoy Mankin <prottoy032020@gmail.com>
2026-05-01 10:14:51 +0600

    Added email password authentication

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,15 +1,50 @@
+"use client";
+
+import { authClient } from "@/lib/auth-client";
+
 const LoginPage = () => {
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+    console.log(email, password);
+
+    const { data, error } = await authClient.signIn.email({
+      email,
+      password,
+    });
+
+    console.log({data, error});
+  }
+
   return (
     <section className="min-h-[80vh] flex flex-col items-center justify-center space-y-10">
         <h2 className="font-bold text-3xl">Please Login</h2>
 
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-            <form className="card-body">
+            <form 
+              className="card-body"
+              onSubmit={onSubmit}
+            >
                 <fieldset className="fieldset">
                 <label className="label text-black">Email</label>
-                <input type="email" className="input" placeholder="Email" />
+                <input 
+                  className="input"
+                  name="email" 
+                  placeholder="Email" 
+                  type="email" 
+                />
+
                 <label className="label text-black">Password</label>
-                <input type="password" className="input" placeholder="Password" />
+                <input 
+                  className="input"
+                  name="password" 
+                  placeholder="Password" 
+                  type="password" 
+                />
+
                 <button type="submit" className="btn bg-black mt-4 text-white">Login</button>
                 </fieldset>
             </form>
--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -1,4 +1,33 @@
+"use client";
+
+import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
+
 const SignUpPage = () => {
+  const router = useRouter();
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    const name = e.target.name.value;
+    const image = e.target.image.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+    console.log({name, image, email, password});
+
+    const { data, error } = await authClient.signUp.email({
+      name,
+      email,
+      password,
+      image,
+    });
+
+    if (!error) {
+      router.push("/");
+    }
+  }
+
   return (
     <section 
       className="min-h-[80vh] flex flex-col items-center justify-center space-y-10"
@@ -7,15 +36,43 @@ const SignUpPage = () => {
       
 
       <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-          <form className="card-body">
+          <form className="card-body" onSubmit={onSubmit}>
             <fieldset className="fieldset">
               <label className="label text-black">Name</label>
-              <input type="name" className="input" placeholder="Name" />
+              <input 
+                name="name" 
+                type="text" 
+                className="input" 
+                placeholder="Name"
+                required 
+              />
+
+              <label className="label text-black">Image URL</label>
+              <input 
+                name="image" 
+                type="text" 
+                className="input" 
+                placeholder="Name"
+              />
 
               <label className="label text-black">Email</label>
-              <input type="email" className="input" placeholder="Email" />
+              <input 
+                name="email" 
+                type="email" 
+                className="input" 
+                placeholder="Email"
+                required 
+              />
+
               <label className="label text-black">Password</label>
-              <input type="password" className="input" placeholder="Password" />
+              <input 
+                name="password" 
+                type="password" 
+                className="input"
+                placeholder="Password"
+                required 
+              />
+
               <button type="submit" className="btn bg-black mt-4 text-white">Login</button>
             </fieldset>
           </form>
